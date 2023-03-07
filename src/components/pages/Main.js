49ac0c8a06685a3dde0bd35f26d7f0c97b5d30f7
limseungchae <tmdco12@naver.com>
2023-03-07 17:17:59 +0900

    Join, Login, List, Myinfo 작성, css 추가

--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,9 +1,19 @@
 import React from "react";
+import {Route, Routes} from "react-router";
+import Home from "./Home";
+import Join from "./Join";
+import Login from "./Login";
+import List from "./List";
+import Myinfo from "./Myinfo";
 const Main = () => {
     return (
-        <main>
-            <img src="/img/smile.png" alt="메인이미지"/>            
-        </main>
+       <Routes>
+           <Route path="/" element={<Home />}></Route>
+           <Route path="/member/join" element={<Join />}></Route>
+           <Route path="/member/login" element={<Login />}></Route>
+           <Route path="/board/list" element={<List />}></Route>
+           <Route path="/member/myinfo" element={<Myinfo />}></Route>
+       </Routes>
     );
 }
 
